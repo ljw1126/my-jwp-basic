773b5028a0c4da79e98feef3c7fe8807f8974f21
leejinwoo <leejinwoo1126@gmail.com>
2023-10-23 17:37:56 +0900

    test: UserDao select/insert/update 테스트

---
+++ src/test/java/next/dao/UserDaoTest.java
@@ -0,0 +1,50 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+import next.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+    @BeforeEach
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void crud() throws SQLException {
+        // 생성
+        User expected = new User("userId", "password", "name", "jinwoo@email.com");
+
+        UserDao dao = new UserDao();
+        dao.insert(expected);
+
+        User actual = dao.findByUserId(expected.getUserId());
+        assertThat(actual).isEqualTo(expected);
+
+        // 수정
+        expected.update(new User("userId", "password2", "name2", "jinwoo@email.com"));
+        dao.update(expected);
+        actual = dao.findByUserId(expected.getUserId());
+        assertThat(actual).extracting("password", "name")
+                .containsExactlyInAnyOrder("password2", "name2");
+    }
+
+    @Test
+    void findAll() throws SQLException {
+        UserDao dao = new UserDao();
+        List<User> userList = dao.findAll();
+        assertThat(userList).hasSize(1);
+    }
+}
