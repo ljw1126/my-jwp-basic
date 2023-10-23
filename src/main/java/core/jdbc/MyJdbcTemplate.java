659dd0f2da28fa77242a111e4208b481db65da1d
leejinwoo <leejinwoo1126@gmail.com>
2023-10-23 18:43:51 +0900

    refactor: MyJdbcTemplate try-with-resources문 사용

--- src/main/java/core/jdbc/MyJdbcTemplate.java
+++ src/main/java/core/jdbc/MyJdbcTemplate.java
@@ -1,27 +1,23 @@
 package core.jdbc;
 
-import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public abstract class MyJdbcTemplate {
-
-    public void update(String query) throws SQLException {
-        Connection con = null;
-        PreparedStatement ps = null;
-        try {
-            con = ConnectionManager.getConnection();
-            ps = con.prepareStatement(query);
-
+    private static final Logger log = LoggerFactory.getLogger(MyJdbcTemplate.class);
+    public void update(String query){
+        try (
+            Connection con = ConnectionManager.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+        ){
             setValues(ps);
-
             ps.executeUpdate();
-        } finally {
-            if(ps != null) ps.close();
-            if(con != null) con.close();
+        } catch (SQLException e) {
+            log.error(e.getMessage());
         }
     }
 
