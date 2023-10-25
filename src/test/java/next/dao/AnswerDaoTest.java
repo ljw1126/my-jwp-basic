df5529ed8568f68c2169b1290c1e7f384c5a94eb
leejinwoo <leejinwoo1126@gmail.com>
2023-10-25 21:51:11 +0900

    test: AnswerDao/QuestionDao 테스트 생성

---
+++ src/test/java/next/dao/AnswerDaoTest.java
@@ -0,0 +1,73 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+import next.model.Answer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswerDaoTest {
+
+    @BeforeEach
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void insert() {
+        //given
+        Date now = new Date();
+        Answer givenAnswer = new Answer(0, "jinwoo", "내용없음체", now, Long.valueOf(1));
+
+        //when
+        AnswerDao answerDao = new AnswerDao();
+        Answer result = answerDao.insert(givenAnswer);
+
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result.getTimeFromCreateDate()).isEqualTo(now.getTime());
+        assertThat(result).extracting("writer", "contents", "questionId")
+                .containsExactlyInAnyOrder("jinwoo", "내용없음체", Long.valueOf(1));
+    }
+
+    @DisplayName("answerId 값으로 답변 Answer를 가져온다")
+    @Test
+    void findById() {
+        //given
+        long answerId = 1L;
+
+        //when
+        AnswerDao answerDao = new AnswerDao();
+        Answer result = answerDao.findById(answerId);
+
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result.getAnswerId()).isEqualTo(answerId);
+    }
+
+    @Test
+    void findAllByQuestionId() {
+        //given
+        long questionId = 7L;
+
+        //when
+        AnswerDao answerDao = new AnswerDao();
+        List<Answer> result = answerDao.findAllByQuestionId(questionId);
+
+        //then
+        assertThat(result).hasSize(2);
+        assertThat(result).extracting("questionId")
+                .containsOnly(questionId);
+    }
+
+}
---
+++ src/test/java/next/dao/QuestionDaoTest.java
@@ -0,0 +1,49 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+import next.model.Question;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuestionDaoTest {
+    @BeforeEach
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void findAll() {
+        //given
+        //when
+        QuestionDao questionDao = new QuestionDao();
+        List<Question> result = questionDao.findAll();
+
+        //then
+        assertThat(result).hasSize(8);
+    }
+
+    @Test
+    void findById() {
+        //given
+        long questionId = 1L;
+
+        //when
+        QuestionDao questionDao = new QuestionDao();
+        Question question = questionDao.findById(questionId);
+
+        //then
+        assertThat(question)
+                .isNotNull()
+                .extracting("questionId", "writer", "title")
+                .containsExactly(questionId, "자바지기", "국내에서 Ruby on Rails와 Play가 활성화되기 힘든 이유는 뭘까?");
+    }
+}
