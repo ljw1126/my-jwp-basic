8ccfff2baa15a41bf552f92477a897b29fa21e1d
leejinwoo <leejinwoo1126@gmail.com>
2023-10-26 16:28:17 +0900

    refactor: View -> ModelAndView 반환 타입 변경

--- src/main/java/core/mvc/JspView.java
+++ src/main/java/core/mvc/JspView.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Set;
 
 public class JspView implements View {
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
@@ -23,10 +25,15 @@ public class JspView implements View {
     }
 
     @Override
-    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         if(viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
             return;
+        }
+
+        Set<String> keys = model.keySet();
+        for(String key : keys) {
+            request.setAttribute(key, model.get(key));
         }
 
         RequestDispatcher rd = request.getRequestDispatcher(viewName);
