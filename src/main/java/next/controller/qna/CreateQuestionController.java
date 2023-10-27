79b4b89c882684223f9752b66b9f451860e4430b
leejinwoo <leejinwoo1126@gmail.com>
2023-10-27 21:19:17 +0900

    feat: 요구사항3. 질문하기 작성, 생성 컨트롤러 추가

---
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -0,0 +1,28 @@
+package next.controller.qna;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+import next.dao.QuestionDao;
+import next.model.Question;
+import next.model.User;
+import next.utils.UserSessionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateQuestionController extends AbstractController {
+    private QuestionDao questionDao = QuestionDao.getInstance();
+
+    @Override
+    public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if(!UserSessionUtils.isLogined(request.getSession())) {
+            return jspView("redirect:/user/loginForm");
+        }
+
+        User user = UserSessionUtils.getUser(request.getSession());
+        Question question = new Question(user.getUserId(), request.getParameter("title"), request.getParameter("contents"));
+        questionDao.insert(question);
+
+        return jspView("redirect:/");
+    }
+}
