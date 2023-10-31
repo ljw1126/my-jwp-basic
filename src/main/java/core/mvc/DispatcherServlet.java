55dd583242cc8c3fb981834755f2f268fb672ee7
leejinwoo <leejinwoo1126@gmail.com>
2023-10-31 16:21:16 +0900

    refactor: RequestMapping클래스명 변경, DispatcherServlet 리팩토링

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -1,7 +1,9 @@
 package core.mvc;
 
+import com.google.common.collect.Lists;
 import core.nmvc.AnnotationHandlerMapping;
 import core.nmvc.HandlerExecution;
+import core.nmvc.HandlerMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,49 +13,61 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private LegacyHandlerMapping lhm;
-    private AnnotationHandlerMapping ahm;
+    private List<HandlerMapping> mappings = Lists.newArrayList();
 
     @Override
     public void init() throws ServletException {
-        lhm = new LegacyHandlerMapping();
+        LegacyHandlerMapping lhm = new LegacyHandlerMapping();
         lhm.initMapping();
-        
-        ahm = new AnnotationHandlerMapping();
+
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping();
         ahm.initialize();
+
+        mappings.add(lhm);
+        mappings.add(ahm);
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestUrl = req.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", req.getMethod(), requestUrl);
+        Object handler = getHandler(req);
+        if(handler == null) {
+            throw new IllegalArgumentException("존재하지 않는 URL입니다");
+        }
 
-        Controller controller = lhm.get(requestUrl);
         try {
-            if(controller != null) {
-                ModelAndView mav = controller.execute(req, resp);
-                render(req, resp, mav);
-            } else {
-                HandlerExecution he = ahm.getHandler(req);
-                if(he == null) {
-                    throw new ServletException("유효하지 않은 요청입니다");
-                }
-
-                render(req, resp, he.handle(req, resp));
-            }
+            ModelAndView mav = execute(handler, req, resp);
+            View view = mav.getView();
+            view.render(mav.getModel(), req, resp);
         } catch (Exception e) {
             log.error("Exception : {}", e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void render(HttpServletRequest req, HttpServletResponse resp, ModelAndView mav) throws Exception {
-        View view = mav.getView();
-        view.render(mav.getModel(), req, resp);
+    private ModelAndView execute(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if(handler instanceof Controller) {
+            return ((Controller)handler).execute(req, resp);
+        } else {
+            return ((HandlerExecution)handler).handle(req, resp);
+        }
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        for(HandlerMapping hm : mappings) {
+            Object handler = hm.getHandler(request);
+            if(handler != null) {
+                return handler;
+            }
+        }
+
+        return null;
     }
 }
--- src/main/java/core/mvc/LegacyHandlerMapping.java
+++ src/main/java/core/mvc/LegacyHandlerMapping.java
@@ -1,5 +1,6 @@
 package core.mvc;
 
+import core.nmvc.HandlerMapping;
 import next.controller.qna.AddAnswerController;
 import next.controller.HomeController;
 import next.controller.qna.ApiDeleteQuestionController;
@@ -20,10 +21,11 @@ import next.controller.user.CreateUserController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LegacyHandlerMapping {
+public class LegacyHandlerMapping implements HandlerMapping {
     private static final Logger log = LoggerFactory.getLogger(LegacyHandlerMapping.class);
 
     private Map<String, Controller> mapping = new HashMap<>();
@@ -56,8 +58,9 @@ public class LegacyHandlerMapping {
         log.info("Init Request Mapping!");
     }
 
-    public Controller get(String requestUrl) {
-        return mapping.get(requestUrl);
+    @Override
+    public Object getHandler(HttpServletRequest request) {
+        return mapping.get(request.getRequestURI());
     }
 
     public void put(String url, Controller controller) {
