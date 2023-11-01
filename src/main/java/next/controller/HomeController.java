b27f8d4d95ac4373c9ce07361cd39fa0ee56dcbb
leejinwoo <leejinwoo1126@gmail.com>
2023-11-01 22:29:31 +0900

    refactor: HomeController 의존성 직접 생성

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -5,13 +5,14 @@ import core.annotation.RequestMapping;
 import core.mvc.ModelAndView;
 import core.nmvc.AbstractNewController;
 import next.dao.JdbcQuestionDao;
+import next.dao.QuestionDao;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController extends AbstractNewController {
-    private JdbcQuestionDao questionDao = JdbcQuestionDao.getInstance();
+    private final QuestionDao questionDao = new JdbcQuestionDao();
 
     @RequestMapping("/")
     public ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception {
