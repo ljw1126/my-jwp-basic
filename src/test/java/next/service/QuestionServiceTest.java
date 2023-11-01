4ed18ecc703eb3985c439f7fb43e7b3f94502d55
leejinwoo <leejinwoo1126@gmail.com>
2023-11-01 22:30:41 +0900

    test: QuestionServiceTest @Mock 테스트 변경

--- src/test/java/next/service/QuestionServiceTest.java
+++ src/test/java/next/service/QuestionServiceTest.java
@@ -1,29 +1,36 @@
 package next.service;
 
+import com.google.common.collect.Lists;
 import core.exception.CannotDeleteException;
 import next.dao.MockAnswerDao;
 import next.dao.MockQuestionDao;
 import next.model.Question;
 import next.model.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class QuestionServiceTest {
-    private QuestionService questionService;
+
+    @Mock
     private MockQuestionDao questionDao;
+
+    @Mock
     private MockAnswerDao answerDao;
 
-    @BeforeEach
-    void setUp() {
-        questionDao = new MockQuestionDao();
-        answerDao = new MockAnswerDao();
-        questionService = QuestionService.getInstance(questionDao, answerDao);
-    }
+    @InjectMocks
+    private QuestionService questionService;
 
     @Test
     void deleteQuestion_없는질문() {
+        when(questionDao.findById(1L)).thenReturn(null);
+
         assertThatThrownBy(() -> questionService.delete(1L, new User("userId")))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("존재하지 않는 글입니다");
@@ -32,7 +39,8 @@ public class QuestionServiceTest {
     @Test
     void deleteQuestion_다른_사용자() {
         Question question = new Question(1L, "jinwoo");
-        questionDao.insert(question);
+        when(questionDao.findById(1L)).thenReturn(question);
+
         assertThatThrownBy(() -> questionService.delete(1L, new User("userId")))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사용자가 쓴 글을 삭제할 수 없습니다");
@@ -42,7 +50,9 @@ public class QuestionServiceTest {
     @Test
     void deleteQuestion_같은_사용자_답변없음() throws Exception {
         Question question = new Question(1L, "jinwoo");
-        questionDao.insert(question);
+        when(questionDao.findById(1L)).thenReturn(question);
+        when(answerDao.findAllByQuestionId(1L)).thenReturn(Lists.newArrayList());
+
         questionService.delete(1L, new User("jinwoo"));
     }
 }
