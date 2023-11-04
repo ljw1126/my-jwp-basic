5b041ddc1316b8d1d2ce060244f066d3305e492c
leejinwoo <leejinwoo1126@gmail.com>
2023-11-04 21:59:57 +0900

    refactor: BeanFactory  초기화시 Inject 인터페이스 사용하도록 변경

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,18 +1,16 @@
 package core.di.factory;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import core.annotation.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 
-import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
+// 빈을 추가하고 조회하는 역할
 public class BeanFactory {
     private static final Logger log = LoggerFactory.getLogger(BeanFactory.class);
 
@@ -20,65 +18,38 @@ public class BeanFactory {
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
+    private List<Injector> injectors;
+
     public BeanFactory(Set<Class<?>> preInstantiatedBeans) {
         this.preInstantiatedBeans = preInstantiatedBeans;
+        this.injectors = Arrays.asList(
+                new ConstructorInjector(this),
+                new FieldInjector(this),
+                new SetterInjector(this)
+        );
     }
 
     public void initialize() {
         for(Class<?> clazz : preInstantiatedBeans) {
             if(beans.get(clazz) == null) {
-                instantiateClass(clazz);
+                inject(clazz);
             }
         }
     }
 
-    // @Inject 있으면 instantiateConstructor()로 생성, 그외는 기본 생성자로 생성
-    private Object instantiateClass(Class<?> clazz) {
-        Object bean = beans.get(clazz);
-        if(bean != null) {
-            return bean;
+    private void inject(Class<?> clazz) {
+        for(Injector injector : injectors) {
+            injector.inject(clazz);
         }
-
-        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
-        if(injectedConstructor == null) {
-            bean = BeanUtils.instantiate(clazz);
-            beans.put(clazz, bean);
-            return bean;
-        }
-
-        log.debug("Constructor : {}", injectedConstructor);
-        bean = instantiateConstructor(injectedConstructor);
-        beans.put(clazz, bean);
-        return bean;
     }
 
-    private Object instantiateConstructor(Constructor<?> constructor) {
-        Class<?>[] parameterTypes = constructor.getParameterTypes();
-        List<Object> args = Lists.newArrayList();
-
-        for(Class<?> clazz : parameterTypes) {
-            Class<?> concertedClass = BeanFactoryUtils.findConcreteClass(clazz, preInstantiatedBeans);
-
-            if(!preInstantiatedBeans.contains(concertedClass)) {
-                throw new IllegalStateException(clazz + "는 Bean 아닙니다");
-            }
-
-            Object bean = beans.get(concertedClass);
-            if(bean == null) {
-                bean = instantiateClass(concertedClass);
-            }
-
-            args.add(bean);
-        }
-
-        return BeanUtils.instantiateClass(constructor, args.toArray());
-    }
-
+    @SuppressWarnings("unchecked")
     public <T> T getBean(Class<T> requiredType) {
         return (T) beans.get(requiredType);
     }
 
     public void clear() {
+        preInstantiatedBeans.clear();
         beans.clear();
     }
 
@@ -92,5 +63,13 @@ public class BeanFactory {
         }
 
         return controllers;
+    }
+
+    public Set<Class<?>> getPreInstantiatedBeans() {
+        return preInstantiatedBeans;
+    }
+
+    public void registerBean(Class<?> clazz, Object instance) {
+        beans.put(clazz, instance);
     }
 }
