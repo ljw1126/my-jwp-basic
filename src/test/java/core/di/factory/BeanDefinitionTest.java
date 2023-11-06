9da79388ef729ef130811956de9fb0e09e0235eb
leejinwoo <leejinwoo1126@gmail.com>
2023-11-06 20:53:49 +0900

    test: BeanDefinitionTest 생성

---
+++ src/test/java/core/di/factory/BeanDefinitionTest.java
@@ -0,0 +1,59 @@
+package core.di.factory;
+
+import core.di.factory.example.JdbcUserRepository;
+import core.di.factory.example.MyQnaService;
+import core.di.factory.example.MyUserController;
+import core.di.factory.example.MyUserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BeanDefinitionTest {
+    private static final Logger log = LoggerFactory.getLogger(BeanScannerTest.class);
+
+    @DisplayName("기본 생성자, 필드/setter, 생성자 주입")
+    @Test
+    void getResolvedAutowiredMode() {
+        BeanDefinition definition = new BeanDefinition(JdbcUserRepository.class);
+        assertThat(definition.getResolvedInjectMode()).isEqualTo(InjectType.INJECT_NO);
+
+        definition = new BeanDefinition(MyUserController.class);
+        assertThat(definition.getResolvedInjectMode()).isEqualTo(InjectType.INJECT_FIELD);
+
+        definition = new BeanDefinition(MyQnaService.class);
+        assertThat(definition.getResolvedInjectMode()).isEqualTo(InjectType.INJECT_CONSTRUCTOR);
+    }
+
+    @DisplayName("setter DI")
+    @Test
+    void getInjectProperties() {
+        BeanDefinition definition = new BeanDefinition(MyUserController.class);
+        Set<Field> injectFields = definition.getInjectFields();
+
+        assertThat(injectFields.size()).isOne();
+
+        for(Field field : injectFields) {
+            log.debug("inject field : {}", field);
+        }
+    }
+
+    @DisplayName("생성자 주입시 Field 사이즈는 0이다")
+    @Test
+    void getConstructor() {
+        BeanDefinition definition = new BeanDefinition(MyQnaService.class);
+        Set<Field> injectFields = definition.getInjectFields();
+
+        assertThat(injectFields).isEmpty();
+
+        Constructor<?> constructor = definition.getInjectConstructor();
+        log.debug("inject constructor : {}", constructor);
+    }
+}
