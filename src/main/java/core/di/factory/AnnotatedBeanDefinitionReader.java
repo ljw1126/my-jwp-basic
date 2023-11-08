a5dec1e6137bcf94d82a81170697a06c09213b07
leejinwoo <leejinwoo1126@gmail.com>
2023-11-08 13:48:52 +0900

    feat: @Configuration 클래스에서 @Bean Method 읽어 -Definition 객체 저장 위임

---
+++ src/main/java/core/di/factory/AnnotatedBeanDefinitionReader.java
@@ -0,0 +1,39 @@
+package core.di.factory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
+// @Configuration + @Bean 읽기 전용
+public class AnnotatedBeanDefinitionReader {
+    private static final Logger log = LoggerFactory.getLogger(AnnotatedBeanDefinitionReader.class);
+
+    private BeanDefinitionRegistry beanDefinitionRegistry;
+
+    public AnnotatedBeanDefinitionReader(BeanDefinitionRegistry beanDefinitionRegistry) {
+        this.beanDefinitionRegistry = beanDefinitionRegistry;
+    }
+
+    public void register(Class<?>... annotatedClasses) {
+        for(Class<?> clazz: annotatedClasses) {
+            registerBean(clazz);
+        }
+    }
+
+    public void registerBean(Class<?> annotatedClazz) {
+        // 1. BeanDefinition 타입 저장 (@Configuration 클래스 저장)
+        beanDefinitionRegistry.registerBeanDefinition(annotatedClazz, new BeanDefinition(annotatedClazz));
+
+        // 2. @Configuration 클래스 안 @Bean Method 저장 - 부모 클래스와 Method 정보 정의 후 @Bean 리턴타입으로 저장
+        Set<Method> beanMethods = BeanFactoryUtils.getBeanMethods(annotatedClazz);
+        for(Method beanMethod : beanMethods) {
+            log.debug("@Bean Method : {}", beanMethod);
+            log.debug("DeclargingClass : {}", beanMethod.getDeclaringClass());
+            log.debug("return type : {}", beanMethod.getReturnType());
+            AnnotatedBeanDefinition abd = new AnnotatedBeanDefinition(beanMethod.getReturnType(), beanMethod);
+            beanDefinitionRegistry.registerBeanDefinition(beanMethod.getReturnType(), abd);
+        }
+    }
+}
