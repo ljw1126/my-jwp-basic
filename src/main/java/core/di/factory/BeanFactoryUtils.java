e65eeee747700ed02acba471941aec63e62b4373
leejinwoo <leejinwoo1126@gmail.com>
2023-11-08 13:58:05 +0900

    update: findConcreteClass() 호출시 Optional 반환하도록 수정

--- src/main/java/core/di/factory/BeanFactoryUtils.java
+++ src/main/java/core/di/factory/BeanFactoryUtils.java
@@ -1,17 +1,25 @@
 package core.di.factory;
 
 import com.google.common.collect.Sets;
+import core.annotation.Bean;
 import core.annotation.Inject;
 import org.reflections.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.reflections.ReflectionUtils.*;
 
 public class BeanFactoryUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(BeanFactoryUtils.class);
 
     @SuppressWarnings({"unchecked"})
     public static Set<Method> getInjectedMethods(Class<?> clazz) {
@@ -26,6 +34,10 @@ public class BeanFactoryUtils {
     @SuppressWarnings({"unchecked", "rawtypes"})
     public static Set<Constructor> getInjectedConstructors(Class<?> clazz) {
         return getAllConstructors(clazz, withAnnotation(Inject.class));
+    }
+
+    public static Set<Method> getBeanMethods(Class<?> clazz) {
+        return getAllMethods(clazz, withAnnotation(Bean.class));
     }
 
     /**
@@ -51,18 +63,27 @@ public class BeanFactoryUtils {
      * @param preInstantiatedBeans
      * @return
      */
-    public static Class<?> findConcreteClass(Class<?> injectedClazz, Set<Class<?>> preInstantiatedBeans){
+    public static Optional<Class<?>> findConcreteClass(Class<?> injectedClazz, Set<Class<?>> preInstantiatedBeans){
         if(!injectedClazz.isInterface()) { // 인터페이스가 아닌 일반 클래스의 경우
-            return injectedClazz;
+            return Optional.of(injectedClazz);
         }
 
         for(Class<?> clazz : preInstantiatedBeans) {
             Set<Class<?>> interfaces = Sets.newHashSet(clazz.getInterfaces()); // clazz 클래스 타입이 가지는 인터페이스 목록 가져옴
             if(interfaces.contains(injectedClazz)) { // 인터페이스 목록에 injectedClazz 인터페이스가 있다면 해당 clazz 반환
-                return clazz;
+                return Optional.of(clazz);
             }
         }
 
         throw new IllegalStateException(injectedClazz + "인터페이스를 구현하는 Bean이 존재하지 않습니다");
+    }
+
+    public static Optional<Object> invokeMethod(Method method, Object bean, Object[] args) {
+        try {
+            return Optional.ofNullable(method.invoke(bean, args));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            log.error(e.getMessage());
+        }
+        return Optional.empty();
     }
 }
